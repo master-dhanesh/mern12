49ed91888e549d1c97b7afe952a463ed4ea1c6f1
master-dhanesh <dhanesh1296@gmail.com>
2024-11-18 20:01:08 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div className="w-[80%] mx-auto rounded mt-10 bg-zinc-200 p-10 ">
+            <h1 className="text-2xl">{import.meta.env.VITE_CHATGPTKEY}</h1>
+            <div>App</div>
+        </div>
+    );
+};
+
+export default App;
