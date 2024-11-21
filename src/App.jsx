4199b28ed7b3fd63a264c0c07fd4e62e98666ccf
master-dhanesh <dhanesh1296@gmail.com>
2024-11-21 20:24:15 +0530

    Two way Binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+
 const App = () => {
+    const [fullname, setfullname] = useState("");
+    const [username, setusername] = useState("");
+
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+        const data = {
+            fullname,
+            username,
+        };
+        console.log(data);
+    };
+
     return (
-        <div className="w-[80%] mx-auto rounded mt-10 bg-zinc-200 p-10 ">
-            <h1 className="text-2xl">{import.meta.env.VITE_CHATGPTKEY}</h1>
-            <div>App</div>
+        <div className="mt-10 w-[80%] mx-auto p-10 rounded bg-zinc-200 ">
+            <h1 className="font-bold text-3xl underline mb-3">Form Handling</h1>
+            <form onSubmit={SubmitHandler}>
+                <input
+                    className="p-2 rounded mr-4 "
+                    type="text"
+                    placeholder="Full Name"
+                    onChange={(e) => setfullname(e.target.value)}
+                    value={fullname}
+                />
+                <input
+                    className="p-2 rounded mr-4 "
+                    type="text"
+                    placeholder="Username"
+                    onChange={(e) => setusername(e.target.value)}
+                    value={username}
+                />
+                <button className="py-2 px-4 bg-zinc-700 rounded text-white">
+                    Submit
+                </button>
+            </form>
         </div>
     );
 };
