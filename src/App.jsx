295d9df0ba5f8ee527c8e3a4efb2c9235f831314
master-dhanesh <dhanesh1296@gmail.com>
2024-11-25 20:28:12 +0530

    react form hook done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useForm } from "react-hook-form";
-// npm i --save react-hook-form
 const App = () => {
-    const { register, handleSubmit } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+            fullname: "John Doe",
+            city: "indore",
+        },
+    });
+
     const submithandler = (data) => {
         console.log(data);
     };
+
     return (
         <div className="mt-10 w-[80%] mx-auto p-10 rounded bg-zinc-200 ">
             <h1 className="font-bold text-3xl underline mb-3">Form Handling</h1>
@@ -13,8 +23,20 @@ const App = () => {
                     className="p-2 rounded mr-4 "
                     type="text"
                     placeholder="Full Name"
-                    {...register("fullname")}
+                    {...register("fullname", {
+                        required: "Full Name is required",
+                    })}
                 />
+
+                {errors.fullname && (
+                    <p className="text-red-500">{errors.fullname.message}</p>
+                )}
+
+                <select {...register("city")}>
+                    <option value="bhopal">Bhopal</option>
+                    <option value="indore">Indore</option>
+                </select>
+
                 <button className="py-2 px-4 bg-zinc-700 rounded text-white">
                     Submit
                 </button>
